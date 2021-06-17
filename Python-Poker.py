2894bf8ea66ca86a439cbbb1bdb9caadfc61b649
dorsi-alt <orsidaniel03@gmail.com>
2021-06-17 19:46:49 -0400

    updates

--- Python-Poker.py
+++ Python-Poker.py
@@ -1,55 +1,83 @@
 import random
 import time
-names = ['John','Dave','Norm','Charles','Hank','Eric','Shaw','']
+names = ['John','Dave','Norm','Charles','Hank','Eric','Shaw','Mike','Kevin','Jane','Monica','Racheal','Pheobe','Janice','Jenn','Katherine']
+
 deck=['A.S','K.S','Q.S','J.S','2.S','3.S','4.S','5.S','6.S','7.S','8.S','9.S','10.S',
         'A.C','K.C','Q.C','J.C','2.C','3.C','4.C','5.C','6.C','7.C','8.C','9.C','10.C',
         'A.H','K.H','Q.H','J.H','2.H','3.H','4.H','5.H','6.H','7.H','8.H','9.H','10.H',
         'A.D','K.D','Q.D','J.D','2.D','3.D','4.D','5.D','6.D','7.D','8.D','9.D','10.D',]
 
+
+tableMaxSize = 4
+potSize = 0
+player1Money = 1000
+player2Money = 1000
+player3Money = 1000
+userMoney = 1000
+playerMoneyArray = [player1Money,player2Money,player3Money,userMoney]
+middleCards = []
+publicTableList = []
 player1Hand = []
 player2Hand = []
 player3Hand = []
-player4Hand = []
+userHand = []
+playerList = []
+
+
+
 
 random.shuffle(deck)
 
-def playerAction():
-    print("What Action Would You Like To Perform")
-    print("1. Call")
-    print("2. Raise")
-    print("3. Check")
-    print("4. Fold")
-
-
-class playerAI:
-    def __init__(self,name,hand):
+class user:
+    def __init__(self,name, hand, money):
         self.name = name
         self.hand = hand
+ 
+    def displayCards(self):
+        print(self.name, self.hand)
 
-def generatePlayers():
-    #creates player 1
-    player1Name  = random.choice(names)
-    player1 = playerAI(player1Name, player1Hand)
+class playerAI:
 
+    def __init__(self,name,hand,money):
+        self.name = name
+        self.hand = hand
+        self.money = money
 
-    #creates player 2
-    player2Name  = random.choice(names)
-    player2 = playerAI(player2Name, player2Hand)
+    def bet(self):
+        time.sleep(1)
+        (self.name, " is make his decision...")
+    
+    def displayCards(self):
+        print(self.name,"?,?")
 
-    #creates player 3
-    player3Name  = random.choice(names)
-    player3 = playerAI(player3Name, player3Hand)
-
+    def makebet():
+        pass
 
 class Table:
-    def __init__(self,p1hand,p2hand,p3hand,p4hand):
-        self.players = 4
+
+    def __init__(self, name, p1hand, p2hand, p3hand, p4hand, seats, publicTableList, middleCards):
+        self.name = name
+        self.seats = seats
         self.p1hand = p1hand
         self.p2hand = p2hand
         self.p3hand = p3hand
         self.p4hand = p4hand
-        self.deck = deck
-    
+        self.playerList = playerList
+        self.publicList = publicTableList
+        self.middleCards = middleCards
+
+    def setPlayers(self):
+
+        self.playerList.append()
+
+    def setStatus(self):
+            self.publicList.append(self.name)
+
+    def playerJoined(self):
+        self.seat -= 1
+        print(self.seats)
+        return(self.seats)        
+
     def dealHands(self):
         #loops for both cards in a hand 
         for x in range (2):
@@ -65,25 +93,72 @@ class Table:
             self.p3hand.append(deck[0])
             deck.remove(deck[0])
 
-            #adds cards to forth players hand and removes the card from the deck 
             self.p4hand.append(deck[0])
             deck.remove(deck[0])
 
-        print(self.p1hand)
-        print(self.p2hand)    
-        print(self.p3hand)
-        print(self.p4hand)
 
-def generateTable():
-    Table1 = Table(player1Hand,player2Hand,player3Hand,player4Hand)
+
+    def dealFlop(self):
+        self.middleCards.append(deck[0])
+        self.middleCards.append(deck[1])
+        self.middleCards.append(deck[2])
+        deck.remove(deck[0])
+        deck.remove(deck[1])
+        deck.remove(deck[2])
+        print(self.middleCards)
+
+
+def Title():
+    global userName
+    print("WELCOME TO TEXAS POKER!")
+    time.sleep(1.5)
+    userName = input("What Is Your Name:\n")
+    time.sleep(1.5)
+    return userName
+
+
+def initGame():
+
+    Title()
+    #generates table object
+    Table1 = Table("danielsTable", player1Hand, player2Hand, player3Hand, userHand, tableMaxSize, publicTableList, middleCards)
+    Table1.setStatus()
+    
+
+    #creates player 1
+    player1Name  = random.choice(names)
+    player1 = playerAI(player1Name, player1Hand,player1Money)
+
+
+    #creates player 2
+    player2Name  = random.choice(names)
+    player2 = playerAI(player2Name, player2Hand,player2Money)
+
+
+    #creates player 3
+    player3Name  = random.choice(names)
+    player3 = playerAI(player3Name, player3Hand,player3Money)
+
+
+    person = user(userName, userHand,userMoney)
+
+    playerList = [player1,player2,player3,person]
+
+
     Table1.dealHands()
+    player1.displayCards()
+    player2.displayCards()
+    player3.displayCards()
+    person.displayCards()
+    Table1.dealFlop()
+
+    
 
 
 
 
 def main():
-    generatePlayers()
-    generateTable()
+    initGame()
 
 
 
